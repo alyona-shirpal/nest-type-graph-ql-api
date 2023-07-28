6c3276a488c0a3cbe8afc5bce461e57aaa757182
jaxin007 <swatdimonbil@gmail.com>
2023-07-28 11:28:46 +0300

    feat [rating] - use Ctx and Authorized

--- src/resolvers/rating.resolver.ts
+++ src/resolvers/rating.resolver.ts
@@ -1,16 +1,31 @@
-import { Arg, Int, Mutation, Resolver } from 'type-graphql';
+import { Arg, Authorized, Ctx, Int, Mutation, Resolver } from 'type-graphql';
 import { Rating } from '../models/Rating';
 import { Book } from '../models/Book';
 import { User } from '../models/User';
+import jwt from 'jsonwebtoken';
+import { MyContext } from '../types/myContext';
 
 @Resolver()
 export class RatingResolver {
+  @Authorized()
   @Mutation(() => Rating)
   async addRating(
     @Arg('bookId', () => Int) bookId: number,
-    @Arg('userId', () => Int) userId: number,
     @Arg('value', () => Int) value: number,
+    @Ctx() context: MyContext,
   ): Promise<Rating> {
+    const token = context.req.headers.authorization;
+
+    if (!token) {
+      throw new Error('Authorization token not provided');
+    }
+
+    const splitToken = token.split(' ')[1];
+
+    const { userId } = jwt.verify(splitToken, process.env.JWT_SECRET) as {
+      userId: number;
+    };
+
     const book = await Book.findOne({ where: { id: Number(bookId) } });
     if (!book) throw new Error('Book is not found');
 
